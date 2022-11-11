d8f3dea48f81af70b735e3ad0579c1545f913c43
LEAH WILNER <leahwilner1@gmail.com>
2022-11-10 20:29:22 -0600

    added console.log and moved index to root

--- js/app.js
+++ js/app.js
@@ -38,6 +38,8 @@ let locLima = {
 
 };
 
+console.log(locSeattle, locTokyo, locDubai, locParis, locLima);
+
 // Problem Domain: the Cedar Rapids Kitten Rescue has tons of kittens who need good homes. One of the best ways to reach prospective adoptive homes is to have profiles for each kitten available on a website. There are hundreds of kittens, though, and only a few volunteers; it's too time-consuming to hand-code each kitten's profile on their website. They need a better way.
 
 //Each Kitten's profile should have:
